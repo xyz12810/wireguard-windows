399916dbdf3583718c0bdb4f59db31ca6ac5bfc7
Markus Hütter <redoced@gmail.com>
2019-02-06 10:26:11 +0100

    got rid of testify package

--- wireguard-manager/gui/TunnelConfiguration_test.go
+++ wireguard-manager/gui/TunnelConfiguration_test.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"net"
+	"reflect"
+	"runtime"
 	"testing"
 
 	b64 "encoding/base64"
-
-	"github.com/stretchr/testify/assert"
 )
 
 const TestInput = `
@@ -38,51 +38,61 @@ func NoError(t *testing.T, err error) bool {
 	if err == nil {
 		return true
 	}
-	t.Error(err)
+	_, fn, line, _ := runtime.Caller(1)
+	t.Errorf("Error at %s:%d: %#v", fn, line, err)
 	return false
 }
 
 func Equal(t *testing.T, expected, actual interface{}) bool {
-	if expected == actual {
+	if reflect.DeepEqual(expected, actual) {
 		return true
 	}
-	t.Errorf("Invalid operation: %#v != %#v (%s)", expected, actual, actual)
-	// exp, ok := expected.([]byte)
-	// if !ok {
-	// 	return reflect.DeepEqual(expected, actual)
-	// }
-	// act, ok := actual.([]byte)
-	// if !ok {
-	// 	return false
-	// }
-	// if exp == nil || act == nil {
-	// 	return exp == nil && act == nil
-	// }
-	// return bytes.Equal(exp, act)
+	_, fn, line, _ := runtime.Caller(1)
+	t.Errorf("Failed equals at %s:%d\nactual   %#v\nexpected %#v", fn, line, expected, actual)
+	return false
+}
+func Len(t *testing.T, actualO interface{}, expected int) bool {
+	actual := reflect.ValueOf(actualO).Len()
+	if reflect.DeepEqual(expected, actual) {
+		return true
+	}
+	_, fn, line, _ := runtime.Caller(1)
+	t.Errorf("Wrong length at %s:%d\nactual   %#v\nexpected %#v", fn, line, expected, actual)
+	return false
+}
+func Contains(t *testing.T, list, element interface{}) bool {
+	listValue := reflect.ValueOf(list)
+	for i := 0; i < listValue.Len(); i++ {
+		if reflect.DeepEqual(listValue.Index(i).Interface(), element) {
+			return true
+		}
+	}
+	_, fn, line, _ := runtime.Caller(1)
+	t.Errorf("Error %s:%d\nelement not found: %#v", fn, line, element)
 	return false
 }
 
 func TestReadTunnelConfiguration(t *testing.T) {
 	conf, err := readTunnelConfiguration(TestInput, "test")
-	if assert.NoError(t, err) {
+	if NoError(t, err) {
 
-		assert.Len(t, conf.wginterface.addresses, 2)
-		assert.Contains(t, conf.wginterface.addresses, IPAddressRange{net.IPv4(10, 10, 0, 1), uint8(16)})
-		assert.Contains(t, conf.wginterface.addresses, IPAddressRange{net.IPv4(10, 192, 122, 1), uint8(24)})
+		Len(t, conf.wginterface.addresses, 2)
+		Contains(t, conf.wginterface.addresses, IPAddressRange{net.IPv4(10, 10, 0, 1), uint8(16)})
+		Contains(t, conf.wginterface.addresses, IPAddressRange{net.IPv4(10, 192, 122, 1), uint8(24)})
 		Equal(t, "yAnz5TF+lXXJte14tji3zlMNq+hd2rYUIgJBgB3fBmk=", b64.StdEncoding.EncodeToString(conf.wginterface.privateKey))
 		Equal(t, uint16(51820), conf.wginterface.listenPort)
 
-		assert.Len(t, conf.peers, 3)
-		assert.Len(t, conf.peers[0].allowedIPs, 2)
+		Len(t, conf.peers, 3)
+		Len(t, conf.peers[0].allowedIPs, 2)
 		Equal(t, Endpoint{iphost: net.IPv4(192, 95, 5, 67), port: 1234}, conf.peers[0].endpoint)
 		Equal(t, "xTIBA5rboUvnH4htodjb6e697QjLERt1NAB4mZqp8Dg=", b64.StdEncoding.EncodeToString(conf.peers[0].publicKey))
 
-		assert.Len(t, conf.peers[1].allowedIPs, 2)
+		Len(t, conf.peers[1].allowedIPs, 2)
 		Equal(t, Endpoint{iphost: net.IP{0x26, 0x07, 0x53, 0, 0, 0x60, 0x6, 0xb0, 0, 0, 0, 0, 0xc0, 0x5f, 0x5, 0x43}, port: 2468}, conf.peers[1].endpoint)
 		Equal(t, "TrMvSoP4jYQlY6RIzBgbssQqY3vxI2Pi+y71lOWWXX0=", b64.StdEncoding.EncodeToString(conf.peers[1].publicKey))
 		Equal(t, uint16(100), conf.peers[1].persistentKeepAlive)
 
-		assert.Len(t, conf.peers[2].allowedIPs, 1)
+		Len(t, conf.peers[2].allowedIPs, 1)
 		Equal(t, Endpoint{host: "test.wireguard.com", port: 18981}, conf.peers[2].endpoint)
 		Equal(t, "gN65BkIKy1eCE9pP1wdc8ROUtkHLF2PfAqYdyYBz6EA=", b64.StdEncoding.EncodeToString(conf.peers[2].publicKey))
 		Equal(t, "TrMvSoP4jYQlY6RIzBgbssQqY3vxI2Pi+y71lOWWXX0=", b64.StdEncoding.EncodeToString(conf.peers[2].preSharedKey))
@@ -99,16 +109,28 @@ func TestParseIPAddressRange(t *testing.T) {
 
 func TestParseEndpoint(t *testing.T) {
 	e, err := parseEndpoint("192.168.42.0:51880")
-	if assert.NoError(t, err) {
+	if NoError(t, err) {
 		Equal(t, net.IPv4(192, 168, 42, 0), e.iphost)
 		Equal(t, "", e.host)
 		Equal(t, uint16(51880), e.port)
 	}
 	e, err = parseEndpoint("test.wireguard.com:18981")
-	if assert.NoError(t, err) {
+	if NoError(t, err) {
 		Equal(t, net.IP(nil), e.iphost)
 		Equal(t, "test.wireguard.com", e.host)
 		Equal(t, uint16(18981), e.port)
+	}
+
+	e, err = parseEndpoint("[2607:5300:60:6b0::c05f:543]:2468")
+	if NoError(t, err) {
+		Equal(t, net.IP{0x26, 0x07, 0x53, 0, 0, 0x60, 0x6, 0xb0, 0, 0, 0, 0, 0xc0, 0x5f, 0x5, 0x43}, e.iphost)
+		Equal(t, "", e.host)
+		Equal(t, uint16(2468), e.port)
+	}
+
+	_, err = parseEndpoint("[::::::invalid:18981")
+	if err == nil {
+		t.Error("Error was expected")
 	}
 
 }
